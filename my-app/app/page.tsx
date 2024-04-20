3554fe3955199983948ff9e07a319b480b98d45b
SouchenOu <soukainaouchenuai@gmail.com>
2024-04-20 03:16:08 -0400

    finish the first part in the firstMain part aand still just the response

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,8 +1,10 @@
+import MainPart1 from "@/components/MainPart1";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
+      <MainPart1/>
     </div>
   );
 }
---
+++ my-app/components/MainPart1.tsx
@@ -0,0 +1,28 @@
+import Image from 'next/image'
+import React from 'react'
+
+const MainPart1 = () => {
+  return (
+    <section className='border-2 border-red-700 flex items-start flex-col py-[30px] px-[80px] w-full gap-[20px]'>
+        <div className='flex flex-col items-center justify-start gap-[20px] w-[600px]'>
+            <h1 className='text-[60px] font-[400] flex-row'>Stay ahead of the curve with our forward-thinking </h1>
+            <p className='text-[20px] font-[300] text-gray-500'>An award-winning SEO agency with disciplines in digital marketing, design, and website development. focused on understanding you.</p>
+        </div>
+        <div className='flex  items-center gap-[20px] '>
+            <button type="button" className='border-2 border-gray-500 p-[20px] text-[20px]  rounded-full text-white bg-black font-bold'>Schedule Call</button>
+            <button type="button" className=' underline p-[20px] text-[20px] font-[400] rounded-full '>View Case Study</button>
+        </div>
+        <div className='flex items-center justify-between gap-[30px]'>
+            <h1 className='text-[20] font-[400] w-[200px]'>Trusted by the world's biggest brands</h1>
+            <Image src="/Companylogo1.png" alt="logo1" width={100} height={90}/>
+            <Image src="/Companylogo2.png" alt="logo2" width={100} height={90}/>
+            <Image src="/Companylogo3.png" alt="logo3" width={100} height={90}/>
+
+        </div>
+      
+      
+    </section>
+  )
+}
+
+export default MainPart1
